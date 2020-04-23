25d9a907c3ee3dce54b27ce64f833526b4345a75
Deep27 <geep2795@gmail.com>
2020-04-23 23:54:19 +0300

    Changed custom extension method name.

--- lib/user_holder.dart
+++ lib/user_holder.dart
@@ -7,8 +7,8 @@ class UserHolder {
   Map<String, User> _users = {};
 
   void registerUser(String username, String phone, String email) {
-    User user =
-        User(name: username.capitalizeName(username), phone: phone, email: email);
+    User user = User(
+        name: username.capitalize(username), phone: phone, email: email);
     if (_users.containsKey(user.login)) {
       throw Exception("User with login ${user.login} already exists!");
     }
@@ -16,8 +16,8 @@ class UserHolder {
   }
 
   User registerUserByEmail(String fullName, String email) {
-    User user =
-        User(name: fullName.capitalizeName(fullName), email: email, phone: null);
+    User user = User(
+        name: fullName.capitalize(fullName), email: email, phone: null);
     if (_users.containsKey(user.login)) {
       throw Exception('A user with this email already exists');
     }
@@ -40,9 +40,10 @@ class UserHolder {
   User getUserByLogin(String login) => _users[login];
 
   User findUserInFriends(String fullName, User user) {
-    User toFindFriendOf = _users[fullName.capitalizeName(fullName)];
+    User toFindFriendOf = _users[fullName.capitalize(fullName)];
     if (toFindFriendOf == null) {
-      throw Exception('No user with name ${fullName.capitalizeName(fullName)}!');
+      throw Exception(
+          'No user with name ${fullName.capitalize(fullName)}!');
     }
     if (toFindFriendOf.friends.contains(user)) {
       return user;
@@ -51,9 +52,11 @@ class UserHolder {
   }
 
   List<User> importUsers(List<String> users) {
-//    users.forEach((userData) => {
-//      List<List<dynamic>> singleUserDate = const CsvToListConverter() .convert(userData);
-//    });
+    users.forEach((userData) {
+      List<List<dynamic>> singleUserDate =
+          const CsvToListConverter(fieldDelimiter: ";").convert(userData);
+    });
+    print("test");
 //    """
 //      Eric Freeman;
 //      eric.freeman@gmail.com;
