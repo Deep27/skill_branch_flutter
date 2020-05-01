ad033f0bf5fac2bdcf71d8e68257284e559dca2f
Deep27 <geep2795@gmail.com>
2020-05-01 23:21:18 +0300

    Prepared FullScreenImage class.

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
-class PhotoScreen extends StatelessWidget {
+class FullScreenImage extends StatelessWidget {
+  final String _name;
+  final String _userName;
+  final String _altDescription;
+
+  FullScreenImage(
+      {String name, String userName, String altDescription, Key key})
+      : _name = name,
+        _userName = userName,
+        _altDescription = altDescription,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container();
