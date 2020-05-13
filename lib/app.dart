e7c904cb6342960b2cbb3c4403d8c1627fa29fd9
Deep27 <geep2795@gmail.com>
2020-05-13 23:10:01 +0300

    Staggered animation.

--- lib/app.dart
+++ lib/app.dart
@@ -11,7 +11,216 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SpinningTextViewExample(),
+      home: StaggeredAnimationExample(),
+    );
+  }
+}
+
+class StaggerAnimation extends StatelessWidget {
+  final Animation<double> controller;
+  final Animation<double> opacity;
+  final Animation<double> width;
+  final Animation<double> height;
+  final Animation<EdgeInsets> padding;
+  final Animation<BorderRadius> borderRadius;
+  final Animation<Color> color;
+  final Animation<double> textOpacity;
+
+  StaggerAnimation({Key key, this.controller})
+      :
+        // Each animation defined here transforms its value during the subset
+        // of the controller's duration defined by the animation's interval.
+        // For example the opacity animation transforms its value during
+        // the first 10% of the controller's duration.
+        opacity = Tween<double>(
+          begin: 0,
+          end: 1,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0,
+              0.1,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        width = Tween<double>(
+          begin: 50,
+          end: 150,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.125,
+              0.250,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        height = Tween<double>(begin: 50, end: 150).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.25,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        padding = EdgeInsetsTween(
+          begin: const EdgeInsets.only(bottom: 16),
+          end: const EdgeInsets.only(bottom: 75),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.25,
+              0.375,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        borderRadius = BorderRadiusTween(
+          begin: BorderRadius.circular(4),
+          end: BorderRadius.circular(75),
+        ).animate(CurvedAnimation(
+          parent: controller,
+          curve: Interval(
+            0.375,
+            0.5,
+            curve: Curves.ease,
+          ),
+        )),
+        color = ColorTween(
+          begin: Colors.indigo[100],
+          end: Colors.white.withOpacity(0.3),
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.5,
+              0.75,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        textOpacity = Tween<double>(
+          begin: 0,
+          end: 1,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.75,
+              1.0,
+              curve: Curves.ease,
+            ),
+          ),
+        ),
+        super(key: key);
+
+  // This function is called each time the controller "ticks" a new frame.
+  // When it runs, all of the animation's values will have been
+  // updated to reflect the controller's current value.
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    return Container(
+      padding: padding.value,
+      alignment: Alignment.bottomCenter,
+      child: Opacity(
+        opacity: opacity.value,
+        child: Container(
+          width: width.value,
+          height: height.value,
+          decoration: BoxDecoration(
+            color: color.value,
+            border: Border.all(
+              color: Colors.indigo[300],
+              width: 3,
+            ),
+            borderRadius: borderRadius.value,
+          ),
+          child: Opacity(
+            opacity: textOpacity.value,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(4),
+                child: Image.network(
+                    'https://skill-branch.ru/_nuxt/img/084fb5f.svg'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: _buildAnimation,
+      animation: controller,
+    );
+  }
+}
+
+class StaggeredAnimationExample extends StatefulWidget {
+  @override
+  _StaggeredAnimationExampleState createState() =>
+      _StaggeredAnimationExampleState();
+}
+
+class _StaggeredAnimationExampleState extends State<StaggeredAnimationExample>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 2000), vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playAnimation() async {
+    try {
+      await _controller.forward().orCancel;
+      await _controller.reverse().orCancel;
+    } on TickerCanceled {
+      // the animation got canceled, probably because we were disposed
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+//    timeDilation = 10; // 1 is normal animation speed
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Staggered Animation'),
+      ),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          _playAnimation();
+        },
+        child: Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.1),
+              border: Border.all(
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+            child: StaggerAnimation(controller: _controller.view),
+          ),
+        ),
+      ),
     );
   }
 }
